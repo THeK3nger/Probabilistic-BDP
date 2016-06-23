ddff4f51773ea1fac215ae2818f93c27dc708de2
Davide Aversa <thek3nger@gmail.com>
2016-06-23 10:19:53 +0200

    Using YoshiX Transformator for Maps
    So that I can keep the object and have a nice representation in the output.

--- pbdp/experiments/optimistic.py
+++ pbdp/experiments/optimistic.py
@@ -7,12 +7,14 @@ class OptimisticPolicyBaseExperiment(YoshiExperiment):
 
     def setup(self):
         self.setup_egg(("Map", "Start", "End", "ExpandedNodes"))
-        self.assign_generators("Map", benchmark.maps_loader())
+        self.assign_generators("Map", benchmark.maps_loader().items())
         self.assign_generators("Start", range(1, 10))
         self.assign_generators("End", range(1,4))
+        self.assign_transformer("Map", lambda x: x[0])
 
     def single_run(self, params):
         print("Single Run {}".format(self.run_counter))
+        print(params["Map"][1].width)
         self.partial_egg["ExpandedNodes"] = 3.45
 
     def after_run(self):
