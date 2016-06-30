cfdd1c104a354d4de052b89d3710b63d2f98d256
Davide Aversa <thek3nger@gmail.com>
2016-06-30 19:17:54 +0200

    Basic YoshiX integration.
    Removed all the old benchmark code that is now YoshiX responsbility.

--- pbdp/experiments/optimistic.py
+++ pbdp/experiments/optimistic.py
@@ -2,6 +2,9 @@ from yoshix import YoshiExperiment
 from yoshix.exporter import YoshiEggCSVExporter
 
 import pbdp.benchmark as benchmark
+from pbdp.mcts.optimistic_policy import OptimisticPolicy
+from pbdp.model.hierarchical_map import HierarchicalMap
+from pbdp.model.agents.agent import VirtualAgent
 
 class OptimisticPolicyBaseExperiment(YoshiExperiment):
 
@@ -14,8 +17,13 @@ class OptimisticPolicyBaseExperiment(YoshiExperiment):
 
     def single_run(self, params):
         print("Single Run {}".format(self.run_counter))
-        print(params["Map"][1].width)
-        self.partial_egg["ExpandedNodes"] = 3.45
+        policy_function = lambda x, y, z, w: OptimisticPolicy.search_path(x, y, z, w, 0.2)
+        map_abstraction = HierarchicalMap(params["Map"][1], 0.2)
+        map_abstraction.generate_abstract_graph()
+        copymap = benchmark.randomize_map(map_abstraction)
+        start, end = benchmark.random_path(params["Map"][1], copymap)
+        virtual_agent = VirtualAgent(copymap, start, end, policy_function)
+        self.partial_egg["ExpandedNodes"] = virtual_agent.profile_data["expanded"]
 
     def after_run(self):
         YoshiEggCSVExporter(self.egg, "badresults.csv").export()
